e69d51893eb7c17df0f1abec9bdbc75c1ad5053d
steven <qscgyj3@gmail.com>
2021-05-12 23:43:37 +0800

    finish the hangman game with adding the button for reset

--- 007_Hangman_Game/script.js
+++ 007_Hangman_Game/script.js
@@ -38,8 +38,30 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-  console.log('Update wrong');
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if(index < errors) {
+      part.style.display= 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+// Check if lost
+  if(wrongLetters.length === figureParts.length){
+    finalMessage.innerText = 'Unfortunately you lost. 😰';
+    popup.style.display = 'flex';
+  }
 }
+
+
 
 // Show notification
 function showNotification() {
@@ -66,15 +88,33 @@ window.addEventListener('keydown', e => {
         showNotification();
       }
     }
-  } else {
+   else {
     if(!wrongLetters.includes(letter)) {
       wrongLetters.push(letter);
 
       updateWrongLettersEl();
     } else {
       showNotification();
+      }
     }
   }
 });
+
+// Restart game and play again
+playAgainBtn.addEventListener('click',() => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
+
+
 
 displayWord();
